079d5e65df19818753b57fc382a418e209c1822e
slavanr45 <slava.r45@gmail.com>
2023-08-28 02:47:12 +0700

    add index, show, new

--- example.py
+++ example.py
@@ -7,12 +7,13 @@ from flask import (
     get_flashed_messages,
     url_for
     )
+import json
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
-
-users = []
+with open('users_db.json', encoding='utf8') as file:
+    users_db = json.load(file)
 
 
 @app.route('/')
@@ -20,54 +21,61 @@ def index():
     return render_template('index.html')
 
 
-# BEGIN (write your solution here)
 @app.route('/users')
-def get_users():
-    messages = get_flashed_messages(with_categories=True)
+def users_get():
+    mes = get_flashed_messages(with_categories=True)
+    users = users_db
     return render_template(
            'users/index.html',
-           users=users,
-           messages=messages)
+           users=users,   messages=mes)
+
+
+@app.route('/users/<int:id>')
+def user_get(id):
+    user = next(filter(lambda x: x['id'] == id, users_db), None)
+    if not user:
+        return 'Page not fount', 404
+    return render_template(
+        'users/show.html',
+        user=user)
 
 
 @app.route('/users/new')
-def new_user():
+def user_new():
     user = {}
-    errors = {}
+    err = {}
     return render_template(
         'users/new.html',
         user=user,
-        errors=errors)
+        err=err)
 
 
-@app.post('/users')
-def post_user():
-    user = request.form.to_dict()
-    errors = validate(user)
-    if errors:
+@app.route('/users', methods=['post'])
+def user_post():
+    data = request.form.to_dict()
+    err = validate(data)  # Проверяем данные
+    if err:
         return render_template(
-          'users/new.html',
-          user=user,
-          errors=errors,), 422
-    users.append(user)
-    flash('User has been created', 'success')
-    return redirect(url_for('get_users'), code=302)
+            'users/new.html',
+            user=data,
+            err=err), 422
+    data['id'] = users_db[-1].get('id', 0) + 1
+    users_db.append(data)
+    with open('users_db.json', 'w', encoding='utf8') as file:
+        json.dump(users_db, file)
+    flash('Новый пользователь был создан', 'success')
+    return redirect(url_for('users_get'))
 
 
-def validate(user):
+def validate(data):
     err = {}
-    if len(user['nickname']) <= 4:
-        err['nickname'] = "Nickname must be greater than 4 characters"
+    if not data['name'] or len(data['name']) < 3:
+        err['name'] = "Can't be blank or short"
+    if not data['email']:
+        err['email'] = "Can't be blank"
     return err
 
 
-# @app.route('/user/<id>/edit')
-# def edit_user(id):   # обработка формы
-#     user = next(filter(lambda x: x['id'] == id, users), None)
-#     err = []
-#     return render_template(
-#            'schools/edit.html',
-#            school=school,
-#            err=err,  )
-
-# END
+@app.route('/users/<id>')
+def user_edit(id):
+    pass
